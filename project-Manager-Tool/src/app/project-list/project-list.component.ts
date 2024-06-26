69e8e6aa272e5d4e42201f569c08fdaa10b8c1b9
rbuwaENG <en97548@foe.sjp.lk>
2024-06-26 14:10:46 +0530

    update

---
+++ project-Manager-Tool/src/app/project-list/project-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-list.component.html',
+  styleUrl: './project-list.component.css'
+})
+export class ProjectListComponent {
+  
+
+}
