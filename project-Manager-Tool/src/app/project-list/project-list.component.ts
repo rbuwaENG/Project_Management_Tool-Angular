56cdab17065e9e820a9c0f530933be358238280a
rbuwaENG <en97548@foe.sjp.lk>
2024-06-26 16:26:03 +0530

    update html

--- project-Manager-Tool/src/app/project-list/project-list.component.ts
+++ project-Manager-Tool/src/app/project-list/project-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectService } from '../services/project.service';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-list.component.html',
-  styleUrl: './project-list.component.css'
+  styleUrls: ['./project-list.component.css']
 })
-export class ProjectListComponent {
-  
+export class ProjectListComponent implements OnInit {
+  projects: Project[] = [];
+  version: string = '';
 
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.projectService.getAll().subscribe(
+      (projects) => {
+        this.projects = projects;
+      },
+      (error) => {
+        console.error('Error fetching projects:', error);
+      }
+    );
+
+  }
 }
---
+++ project-Manager-Tool/src/app/services/project.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Project } from '../models/project.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private projectsUrl = 'assets/projects.json'; // Path to your JSON file
+
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Project[]> {
+    return this.http.get<any>(this.projectsUrl).pipe(
+      map(response => response.projects as Project[])
+    );
+  }
+
+ 
+  readJson(filePath: string): Observable<any> {
+    return this.http.get<any>(this.projectsUrl + filePath);
+  }
+}
