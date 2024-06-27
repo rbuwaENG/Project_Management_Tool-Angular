be56b3533c641ad17438d046529020f550042f0c
rbuwaENG <en97548@foe.sjp.lk>
2024-06-27 10:14:04 +0530

    update app.module

--- project-Manager-Tool/src/app/app.module.ts
+++ project-Manager-Tool/src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectService } from './services/project.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProjectListComponent
-  ],
+
   imports: [
     BrowserModule,
     CommonModule,
-    HttpClientModule
+   
   ],
   providers: [ProjectService],
-  bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
